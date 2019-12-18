518ac3673003c3737e0a0cffe291eef69b913736
swtpumpkin <swtpumpkin90@gmail.com>
2019-12-18 18:15:26 +0900

    Add Type of Function Results

--- typescript-express-starter/lib/project-files/src/controllers/users.controller.ts
+++ typescript-express-starter/lib/project-files/src/controllers/users.controller.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { CreateUserDto } from '../dtos/users.dto';
-import { User } from '../interfaces/users.interface';
+import { User, Users } from '../interfaces/users.interface';
 import userService from '../services/users.service';
 
 class UsersController {
@@ -8,7 +8,7 @@ class UsersController {
 
   public getUsers = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const findAllUsersData = await this.userService.findAllUser();
+      const findAllUsersData: Users = await this.userService.findAllUser();
       res.status(200).json({ data: findAllUsersData, message: 'findAll' });
     } catch (error) {
       next(error);
@@ -19,7 +19,7 @@ class UsersController {
     const userId: number = Number(req.params.id);
 
     try {
-      const findOneUserData = await this.userService.findUserById(userId);
+      const findOneUserData: User = await this.userService.findUserById(userId);
       res.status(200).json({ data: findOneUserData, message: 'findOne' });
     } catch (error) {
       next(error);
@@ -30,7 +30,7 @@ class UsersController {
     const userData: CreateUserDto = req.body;
 
     try {
-      const createUserData = await this.userService.createUser(userData);
+      const createUserData: User = await this.userService.createUser(userData);
       res.status(201).json({ data: createUserData, message: 'created' });
     } catch (error) {
       next(error);
@@ -42,7 +42,7 @@ class UsersController {
     const userData: User = req.body;
 
     try {
-      const updateUserData = await this.userService.updateUser(userId, userData);
+      const updateUserData: User = await this.userService.updateUser(userId, userData);
       res.status(200).json({ data: updateUserData, message: 'updated' });
     } catch (error) {
       next(error);
@@ -53,7 +53,7 @@ class UsersController {
     const userId: number = Number(req.params.id);
 
     try {
-      const deleteUserData = await this.userService.deleteUserData(userId);
+      const deleteUserData: Users = await this.userService.deleteUserData(userId);
       res.status(200).json({ data: deleteUserData, message: 'deleted' });
     } catch (error) {
       next(error);
