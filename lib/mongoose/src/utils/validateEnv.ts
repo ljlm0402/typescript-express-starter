faff43bb3d167c3ae456e2a565895938538a7546
임경민 <ljlm0402@naver.com>
2020-12-03 17:45:45 +0900

    ✨ Add Feature
    > ✨ Add Feature in monggose
    - Add database connection file
    - Fix package description

--- lib/mongoose/src/utils/validateEnv.ts
+++ lib/mongoose/src/utils/validateEnv.ts
@@ -3,11 +3,11 @@ import { cleanEnv, port, str } from 'envalid';
 const validateEnv = () => {
   cleanEnv(process.env, {
     NODE_ENV: str(),
+    PORT: port(),
     MONGO_HOST: str(),
     MONGO_PORT: str(),
     MONGO_DATABASE: str(),
     JWT_SECRET: str(),
-    PORT: port(),
   });
 };
 
