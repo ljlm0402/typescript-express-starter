f532e7aa3f11e25cc3a0e364a98a2a1e04d07555
임경민 <ljlm0402@naver.com>
2021-12-23 20:20:56 +0900

    🛠 Refactoring Code
    - Fix named export by 'bcrypt', 'fs', 'jsonwebtoken', 'path'
    - Fix authMiddleware
    - import index - set env 'NODE_CONFIG_DIR'
    - Fix env, docker-compose enviroment

--- lib/sequelize/src/services/auth.service.ts
+++ lib/sequelize/src/services/auth.service.ts
@@ -1,6 +1,6 @@
-import bcrypt from 'bcrypt';
+import { compare, hash } from 'bcrypt';
 import config from 'config';
-import jwt from 'jsonwebtoken';
+import { sign } from 'jsonwebtoken';
 import DB from '@databases';
 import { CreateUserDto } from '@dtos/users.dto';
 import { HttpException } from '@exceptions/HttpException';
@@ -17,7 +17,7 @@ class AuthService {
     const findUser: User = await this.users.findOne({ where: { email: userData.email } });
     if (findUser) throw new HttpException(409, `You're email ${userData.email} already exists`);
 
-    const hashedPassword = await bcrypt.hash(userData.password, 10);
+    const hashedPassword = await hash(userData.password, 10);
     const createUserData: User = await this.users.create({ ...userData, password: hashedPassword });
 
     return createUserData;
@@ -29,7 +29,7 @@ class AuthService {
     const findUser: User = await this.users.findOne({ where: { email: userData.email } });
     if (!findUser) throw new HttpException(409, `You're email ${userData.email} not found`);
 
-    const isPasswordMatching: boolean = await bcrypt.compare(userData.password, findUser.password);
+    const isPasswordMatching: boolean = await compare(userData.password, findUser.password);
     if (!isPasswordMatching) throw new HttpException(409, "You're password not matching");
 
     const tokenData = this.createToken(findUser);
@@ -52,7 +52,7 @@ class AuthService {
     const secretKey: string = config.get('secretKey');
     const expiresIn: number = 60 * 60;
 
-    return { expiresIn, token: jwt.sign(dataStoredInToken, secretKey, { expiresIn }) };
+    return { expiresIn, token: sign(dataStoredInToken, secretKey, { expiresIn }) };
   }
 
   public createCookie(tokenData: TokenData): string {
