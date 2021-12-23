1110d4e837098a473f3ff54ac6052e824a9dfcd1
임경민 <ljlm0402@naver.com>
2021-12-23 20:20:56 +0900

    🛠 Refactoring Code
    - Fix named export by 'bcrypt', 'fs', 'jsonwebtoken', 'path'
    - Fix authMiddleware
    - import index - set env 'NODE_CONFIG_DIR'
    - Fix env, docker-compose enviroment

--- lib/sequelize/src/middlewares/auth.middleware.ts
+++ lib/sequelize/src/middlewares/auth.middleware.ts
@@ -1,17 +1,17 @@
-import { NextFunction, Response } from 'express';
 import config from 'config';
-import jwt from 'jsonwebtoken';
+import { NextFunction, Response } from 'express';
+import { verify } from 'jsonwebtoken';
 import DB from '@databases';
 import { HttpException } from '@exceptions/HttpException';
 import { DataStoredInToken, RequestWithUser } from '@interfaces/auth.interface';
 
 const authMiddleware = async (req: RequestWithUser, res: Response, next: NextFunction) => {
   try {
-    const Authorization = req.cookies['Authorization'] || req.header('Authorization').split('Bearer ')[1] || null;
+    const Authorization = req.cookies['Authorization'] || (req.header('Authorization') ? req.header('Authorization').split('Bearer ')[1] : null);
 
     if (Authorization) {
       const secretKey: string = config.get('secretKey');
-      const verificationResponse = jwt.verify(Authorization, secretKey) as DataStoredInToken;
+      const verificationResponse = verify(Authorization, secretKey) as DataStoredInToken;
       const userId = verificationResponse.id;
       const findUser = await DB.Users.findByPk(userId);
 
