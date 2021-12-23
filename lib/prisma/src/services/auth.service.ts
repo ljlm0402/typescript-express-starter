05d36c116d5801fa0765572291602e000d1b057f
임경민 <ljlm0402@naver.com>
2021-12-23 19:21:00 +0900

    🛠 Refactoring Code
    - Fix named export by 'bcrypt', 'fs', 'jsonwebtoken', 'path'
    - Fix authMiddleware
    - import index - set env 'NODE_CONFIG_DIR'
    - Fix env, docker-compose enviroment

--- lib/prisma/src/services/auth.service.ts
+++ lib/prisma/src/services/auth.service.ts
@@ -1,6 +1,6 @@
-import bcrypt from 'bcrypt';
+import { compare, hash } from 'bcrypt';
 import config from 'config';
-import jwt from 'jsonwebtoken';
+import { sign } from 'jsonwebtoken';
 import { PrismaClient, User } from '@prisma/client';
 import { CreateUserDto } from '@dtos/users.dto';
 import { HttpException } from '@exceptions/HttpException';
@@ -16,7 +16,7 @@ class AuthService {
     const findUser: User = await this.users.findUnique({ where: { email: userData.email } });
     if (findUser) throw new HttpException(409, `You're email ${userData.email} already exists`);
 
-    const hashedPassword = await bcrypt.hash(userData.password, 10);
+    const hashedPassword = await hash(userData.password, 10);
     const createUserData: Promise<User> = this.users.create({ data: { ...userData, password: hashedPassword } });
 
     return createUserData;
@@ -28,7 +28,7 @@ class AuthService {
     const findUser: User = await this.users.findUnique({ where: { email: userData.email } });
     if (!findUser) throw new HttpException(409, `You're email ${userData.email} not found`);
 
-    const isPasswordMatching: boolean = await bcrypt.compare(userData.password, findUser.password);
+    const isPasswordMatching: boolean = await compare(userData.password, findUser.password);
     if (!isPasswordMatching) throw new HttpException(409, "You're password not matching");
 
     const tokenData = this.createToken(findUser);
@@ -51,7 +51,7 @@ class AuthService {
     const secretKey: string = config.get('secretKey');
     const expiresIn: number = 60 * 60;
 
-    return { expiresIn, token: jwt.sign(dataStoredInToken, secretKey, { expiresIn }) };
+    return { expiresIn, token: sign(dataStoredInToken, secretKey, { expiresIn }) };
   }
 
   public createCookie(tokenData: TokenData): string {
