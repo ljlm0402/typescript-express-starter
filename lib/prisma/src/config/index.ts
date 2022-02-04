43e7182465a090a93891c88bcd1897cddd75c2a1
임경민 <ljlm0402@naver.com>
2022-02-04 18:45:53 +0900

    ✨ Add Feature
    - dotenv file management

---
+++ lib/prisma/src/config/index.ts
@@ -0,0 +1,5 @@
+import { config } from 'dotenv';
+config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
+
+export const CREDENTIALS = process.env.CREDENTIALS === 'true';
+export const { NODE_ENV, PORT, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN } = process.env;
