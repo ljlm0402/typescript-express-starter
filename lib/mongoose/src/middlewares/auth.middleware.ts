c794e747f0ba7a6f419000b11e76ccee5403f7d5
Abdullah Hesham <abdullahhesham@protonmail.com>
2024-01-27 10:08:37 +0000

    fix: import path typo

--- lib/mongoose/src/middlewares/auth.middleware.ts
+++ lib/mongoose/src/middlewares/auth.middleware.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 import { SECRET_KEY } from '@config';
-import { HttpException } from '@exceptions/httpException';
+import { HttpException } from '@exceptions/HttpException';
 import { DataStoredInToken, RequestWithUser } from '@interfaces/auth.interface';
 import { UserModel } from '@models/users.model';
 
